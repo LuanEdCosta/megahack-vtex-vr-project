fc16d5f37786392a2a3ff8a6dfe6b96608f76693
Luan Eduardo da Costa <luan1346@gmail.com>
2020-04-30 02:24:29 -0300

    Creating the show-element-on-hover component

--- src/components/ShowElementOnHover.js
+++ src/components/ShowElementOnHover.js
@@ -1,6 +1,24 @@
-AFRAME.registerComponent('foo', {
+AFRAME.registerComponent('show-element-on-hover', {
   schema: {
-    show: { default: 'false', type: 'boolean' }
+    element: { default: null, type: 'selector' },
+    showOnInit: { default: false, type: 'boolean' }
   },
-  init: function () {}
+  init: function () {
+    var element = this.data.element
+    var showOnInit = this.data.showOnInit
+
+    element.object3D.visible = showOnInit
+
+    this.el.addEventListener('mouseenter', (e) => {
+      if (element) {
+        element.object3D.visible = true
+      }
+    })
+
+    this.el.addEventListener('mouseleave', () => {
+      if (element) {
+        element.object3D.visible = false
+      }
+    })
+  }
 })
