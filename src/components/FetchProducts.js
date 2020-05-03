d13f736e3e0f51bdf2e5801de63bad9260c78d5a
Luis Otávio <luis.otavio2425@gmail.com>
2020-05-03 13:27:01 -0300

    Fetch API Products

--- src/components/FetchProducts.js
+++ src/components/FetchProducts.js
@@ -1,60 +1,22 @@
+
+
 AFRAME.registerComponent('fetch-products', {
   init: function () {
-    var data = {
-      afehaoiueaefh: {
-        id: 'afehaoiueaefh',
-        name: 'Tenis 1',
-        description: 'Um produto legal',
-        price: 500,
-        'gltf-model': '#tennis',
-        position: '-1.5 1.45 -3',
-        scale: '0.4 0.4 0.4',
-        animation: {
-          property: 'rotation',
-          to: '0 360 0',
-          dir: 'alternate',
-          dur: '5000',
-          loop: 'true',
-          easing: 'linear'
-        }
-      },
-      feuaefubfaiobn: {
-        id: 'feuaefubfaiobn',
-        name: 'Tenis 1',
-        description: 'Um produto legal',
-        price: 500,
-        'gltf-model': '#tennis',
-        position: '0 1.45 -3',
-        scale: '0.4 0.4 0.4',
-        animation: {
-          property: 'rotation',
-          to: '0 360 0',
-          dir: 'alternate',
-          dur: '5000',
-          loop: 'true',
-          easing: 'linear'
-        }
-      },
-      asdofihaoifha: {
-        id: 'asdofihaoifha',
-        name: 'Tenis 1',
-        description: 'Um produto legal',
-        price: 500,
-        'gltf-model': '#tennis',
-        position: '1.5 1.45 -3',
-        scale: '0.4 0.4 0.4',
-        animation: {
-          property: 'rotation',
-          to: '0 360 0',
-          dir: 'alternate',
-          dur: '5000',
-          loop: 'true',
-          easing: 'linear'
-        }
-      }
-    }
 
-    localStorage.setItem('PRODUCTS', JSON.stringify(data))
-    renderProducts(data)
+    axios.get('https://api-hackaton.herokuapp.com/products')
+    .then(function (response) {
+
+      console.log(response.data);
+      localStorage.setItem('PRODUCTS', JSON.stringify(response.data))
+      renderProducts(response.data)
+
+    })
+    .catch(function (error) {
+      return error;
+    });
+
+
+
+
   }
 })
